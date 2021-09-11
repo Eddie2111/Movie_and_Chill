ca4a6173fc5fc086a33b617ec19e2f4e3a35dfee
unknown <tarek42223@gmail.com>
2021-09-12 04:08:59 +0600

    Second Commit

--- testapp.py
+++ testapp.py
@@ -1,48 +1,116 @@
-import random,requests,os
-from tkinter import *
+from random import randint
+from requests import get 
+from tkinter.ttk import Progressbar
+from tkinter import Label, Button, Tk, ttk, Frame
 from bs4 import BeautifulSoup
 from io import BytesIO
 from PIL import ImageTk, Image
+import time
+
+pro = ttk.Progressbar
+w=Tk()
 
 url = 'https://www.imdb.com/chart/top'
 List_main = []
 img = ''
+
 def start():
     global List_main
-    response = requests.get(url)
-    html = response.text
-    soup = BeautifulSoup(html,'html.parser')
-    movietags = soup.select ('td.titleColumn')
-    List_main = movietags
+    if len(List_main)==0:
+        response = get(url)
+        html = response.text
+        soup = BeautifulSoup(html,'html.parser')
+        movietags = soup.select ('td.titleColumn')
+        List_main = movietags
 def out():
-    movietag0 = List_main[random.randint(1,249)]
+    movietag0 = List_main[randint(1,249)]
     moviesplit = movietag0.text.split()
     moviesplit.pop(0)
     a = ' '.join(moviesplit)
     return a
 def click():
-    label0=Label(root,text='{}'.format(out()),bg= '#488ddb',font = ('Tahoma','11'))
+    label0=Label(w,text='{}'.format(out()),bg= '#488ddb',font = ('Tahoma','11'))
     label0.pack()
-def imgf():
-    global img
-    img_url = "https://cdn.radiofrance.fr/s3/cruiser-production/2020/09/e60f0dcf-46ad-4289-aa05-61230e7c3926/838_gettyimages-566435049.webp"
-    response = requests.get(img_url)
-    img_data = response.content
-    img = ImageTk.PhotoImage(Image.open(BytesIO(img_data)))
+
+width_of_window = 427
+height_of_window = 250
+screen_width = w.winfo_screenwidth()
+screen_height = w.winfo_screenheight()
+x_coordinate = (screen_width/2)-(width_of_window/2)
+y_coordinate = (screen_height/2)-(height_of_window/2)
+w.geometry("%dx%d+%d+%d" %(width_of_window,height_of_window,x_coordinate,y_coordinate))
+
+w.overrideredirect(1)
+
+s = ttk.Style()
+s.theme_use('clam')
+s.configure("red.Horizontal.TProgressbar", foreground='red', background='#07fa18')
+progress=pro(w,style="red.Horizontal.TProgressbar",orient='horizontal',length=500,mode='determinate',)
+
+#############  progressbar   33333333333333333333333333333
+def new_win():
+    root = Tk()
+    root.minsize(300, 200);root.maxsize(500, 400);
+    root.title('Movie Suggester')
+
+    ##background##
+    #imgf()
+    #backpanel = Label(root, image=img)
+    #backpanel.pack(side="bottom", fill="both")
+    ##background##
+
+    label0 = Label(root,text='Click the button to get a movie suggestion..!',font=('Ariel','15'),fg='orange')
+    label0.pack();
+    button0 = Button(root,activeforeground= 'red', activebackground='blue', width=42,height=2, text="Click me",command=click,fg='white',bg='black',font=('Helvetica','10'))
+    button0.pack();
+    
+    root.mainloop()
+
+def bar():
+    l4=Label(w,text='Loading...',fg='white',bg=a)
+    lst4=('Calibri (Body)',10)
+    l4.config(font=lst4)
+    l4.place(x=18,y=210)
+    import time
+    r=0
+    for i in range(100):
+        progress['value']=r
+        w.update_idletasks()
+        if len(List_main)==0:
+            start()
+        if len(List_main)!=0:pass;
+        time.sleep(0.03)
+        r=r+1
+    w.destroy()
+        
+progress.place(x=-10,y=235)
 
 
-root = Tk()
-root.minsize(300, 200);root.maxsize(500, 400);
-root.title('Movie Suggester')
 
-##background##
-imgf()
-backpanel = Label(root, image=img)
-#backpanel.pack(side="bottom", fill="both")
-##background##
-label0 = Label(root,text='Click the button to get a movie suggestion..!',font=('Ariel','15'),fg='orange')
-label0.pack();start();
-button0 = Button(root,activeforeground= 'red', activebackground='blue', width=42,height=2, text="Click me",command=click,fg='white',bg='black',font=('Helvetica','10'))
-button0.pack();
+a='#0a52f0'
+Frame(w,width=427,height=241,bg=a).place(x=0,y=0)  #249794
+b1=Button(w,width=10,height=1,text='Get Started',command=bar,border=0,fg=a,bg='white')
 
-root.mainloop()
+b1.place(x=170,y=200)
+
+
+######## Label
+
+l1=Label(w,text='Movie',fg='white',bg=a)
+lst1=('Calibri (Body)',18,'bold')
+l1.config(font=lst1)
+l1.place(x=50,y=80)
+l2=Label(w,text='Suggester',fg='white',bg=a)
+lst2=('Calibri (Body)',18,'bold')
+l2.config(font=lst2)
+l2.place(x=123,y=80)
+
+l3=Label(w,text='',fg='white',bg=a)
+lst3=('Calibri (Body)',13)
+l3.config(font=lst3)
+l3.place(x=50,y=110)
+w.mainloop()
+
+new_win()
+
+
